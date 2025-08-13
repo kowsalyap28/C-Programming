ebcc8fabc84ac69ae5b5e3063e6b5c8df2214da2
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-08-13 22:29:10 +0530

    created if_statement

---
+++ 07.if_statement/exercise/01.problem.c
@@ -0,0 +1,20 @@
+// checking even or odd
+
+# include <stdio.h>
+void main()
+{
+
+int a;
+printf("entre the variable a:");
+scanf("%d",&a);
+
+if(a%2==0)
+{
+    printf("a is even");
+}
+else
+{
+
+    printf("a is odd");
+}
+}
---
+++ 07.if_statement/exercise/02.problem.c
@@ -0,0 +1,22 @@
+// positive negative 
+# include <stdio.h>
+void main()
+{
+int a;
+printf("entre the variable a:");
+scanf("%d",&a);
+if(a==0)
+{
+
+    printf("a is neutral");
+}
+else if(a<0)
+{
+    printf("a is negative");
+}
+else
+{
+    printf(" a is positive");
+}
+
+}
