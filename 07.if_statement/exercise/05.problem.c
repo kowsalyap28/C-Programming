c72539316be9f4a06cf251bc2576e3b6894a3e6c
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-08-15 21:22:05 +0530

    created if_statement

---
+++ 07.if_statement/exercise/05.problem.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+void main()
+{
+int age;
+printf("entre your age:");
+scanf("%d",&age);
+
+if(age>18)
+{
+    printf("you are elegible for voteing");
+
+}
+
+else 
+{
+printf("you are not eligible ");
+
+}
+}
