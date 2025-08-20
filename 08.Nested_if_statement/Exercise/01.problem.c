5d5e2641093d56f8bfef52f380c1a2cc2cb1b523
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-08-20 22:41:55 +0530

    created nested if statement

---
+++ 08.Nested_if_statement/Exercise/01.problem.c
@@ -0,0 +1,28 @@
+// Neasted if
+
+# include <stdio.h>
+void main()
+{
+int a;
+printf("entre the variable a:");
+scanf("%d",&a);
+if (a>=1)
+{
+      if(a>5)
+
+    {
+        printf("a is greater than 5");
+    }
+     
+     else
+     {
+        printf("a is less than 5 ");
+     }
+}
+
+else
+{
+    printf("a is less than 1");
+}
+
+}
