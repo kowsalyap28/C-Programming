ae7c9639e7cacb26250f1d8928dc35ebd0dcb081
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-08-23 23:24:47 +0530

    created Nested if staement

--- 08.Nested_if_statement/Exercise/03.problem.c
+++ 08.Nested_if_statement/Exercise/03.problem.c
@@ -1,11 +1,24 @@
-// leap year using and
+// checkinf a number is positive or negitive if positive check even or odd
 # include <stdio.h>
-void mian()
+int mian()
 {
-int year;
-printf("entre the year:");
-scanf("%d",&year);
+int a;
+printf("entre the a :");
+scanf("%d",&a);
 
-
-
+if(a>0)
+{
+    if(a%2==0)
+    { 
+        printf(" a is even");
+    }
+    else
+    {
+        printf("a is odd");
+    }
+}
+else
+{
+    printf(" a is negative");
+}
 }
