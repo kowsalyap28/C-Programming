9d7c45b173b9eef75f1d769365dbf50856ba4b5a
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-08-23 23:06:29 +0530

    created switch case syntax

---
+++ 08.Nested_if_statement/Exercise/03.problem.c
@@ -0,0 +1,11 @@
+// leap year using and
+# include <stdio.h>
+void mian()
+{
+int year;
+printf("entre the year:");
+scanf("%d",&year);
+
+
+
+}
---
+++ 11.problem_solvings_in_C/02.problem.c
@@ -0,0 +1,5 @@
+// problem 2: Area of circle
+# include <stdio.h>
+void main()
+{
+}
