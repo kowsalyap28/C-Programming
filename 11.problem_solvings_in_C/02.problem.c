baada255f50afef866f681aa2084c173c2d133f1
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-08-24 21:58:20 +0530

    solved problem 2:  Area of circle

--- 11.problem_solvings_in_C/02.problem.c
+++ 11.problem_solvings_in_C/02.problem.c
@@ -2,4 +2,16 @@
 # include <stdio.h>
 void main()
 {
+float radius,area;
+    printf("enter the radius of cirle:");
+    scanf("%f",&radius);
+if(radius<=0)
+{
+    printf("give the radius in positive:");
+}
+else
+{
+    area=3.14*radius*radius;
+    printf("area of circle=%f",area);
+}
 }
