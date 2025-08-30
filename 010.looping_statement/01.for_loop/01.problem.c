a46bf3b959dacd176e9d51a70db81446fc8ab72e
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-08-30 23:16:58 +0530

    created for loop : sum of digitd

---
+++ 010.looping_statement/01.for_loop/01.problem.c
@@ -0,0 +1,15 @@
+  // sum of digits in for loop
+  # include <stdio.h>
+  void main()
+  {
+   int i,n,sum=0;
+   printf("entre the value of n:");
+   scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    sum= sum+i;
+  }
+  printf("sum=%d",sum);
+
+  }
+
