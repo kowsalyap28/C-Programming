c65f87790f1002435c1193b0b46386aa0ac4324b
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-09-16 23:33:22 +0530

    solved problem 31: type conversion

---
+++ z.11.problem_solvings_in_C/31.problem.c
@@ -0,0 +1,16 @@
+// Data type conversion 
+# include <stdio.h>
+void main()
+{
+    char ch1;
+    printf("enter the character: ");
+    scanf("%c",&ch1);
+    printf("ch=%c\n",ch1);
+    int a=ch1;
+    printf("a=%d\n",a);
+    float f=a;
+    printf("f=%f\n",f);
+    double lf=f;
+    printf("lf=%lf",f);
+
+}
