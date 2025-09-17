626a10949257d16ac2519a6eea47adbde8867ecf
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-09-17 22:38:05 +0530

    solved problem 29:grade in switch case

---
+++ z.11.problem_solvings_in_C/29.problem.c
@@ -0,0 +1,29 @@
+// write a programm to  check in switch case to check grade
+
+
+# include <stdio.h>
+void main()
+{
+   int grade;
+   printf("entre the marks");
+   scanf("%d",&grade);
+
+ switch(grade)
+ {
+  case 1:
+  printf("grade A");
+  break;
+ case 2:
+ printf("grade B");
+ break;
+ case 3:
+  printf("grade c");
+  break;
+  case 4:
+  printf("grade d");
+  break;
+
+ }
+
+
+}
