de4c63d89191f8a7026f065b4b62f2a7ec8eaea8
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-10-05 10:43:35 +0530

    solved problem57:both inputs are equal of not

---
+++ z.11.problem_solvings_in_C/57.problem.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+  if(a==b)
+  {
+    printf("both inputs are equal");
+  }   
+  else
+  {
+    printf("both the inputs are not equal");
+  }
+}
