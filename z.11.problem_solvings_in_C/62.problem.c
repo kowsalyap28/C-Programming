bea6588890164af4cc68a8339ab45719459e7978
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-10-06 20:32:11 +0530

    solved problem62:total amoount

---
+++ z.11.problem_solvings_in_C/62.problem.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main()
+{
+ int pen,notebook,pencils,amount;
+ scanf("%d %d %d",&pen,&notebook,&pencils);
+  amount=(pen*20)+(notebook*70)+(pencils*9);
+printf("%d",amount);
+}
