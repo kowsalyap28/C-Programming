53cbe39d7d9a36d27ba24bf33a4c3d35b8094c6c
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-10-12 22:04:11 +0530

    created

---
+++ z.11.problem_solvings_in_C/63.problem.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main()
+{
+  char ch1,ch2;
+  scanf("%c %c",&ch1,&ch2);
+   int a=ch1,b=ch2;
+  if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='U'||ch=='I'||ch=='o'||ch=='u')
+
+    
+}
