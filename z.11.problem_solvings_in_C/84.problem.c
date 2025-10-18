68da6c0335141eb84c5dc6e2139e0a9ba1d878b8
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-10-18 23:59:02 +0530

    solved problem83:array largest num

---
+++ z.11.problem_solvings_in_C/84.problem.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+int main()
+{
+    int num[10],i,n;
+    int largest;
+    scanf("%d",&n);  // n is how many num ther user want to give the input and ger the output
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&num[i]);
+    }
+    largest = num[0];
+    for(i=1;i<n;i++)
+    {
+        if(num[i]>largest)
+        {
+           largest = num[i];
+        }
+    }
+    printf("largest=%d",largest);
+}
