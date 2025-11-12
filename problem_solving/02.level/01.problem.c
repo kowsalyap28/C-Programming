572c52f2e9e8fbe2f5476aa96bb076ced5dde984
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-11-12 23:04:33 +0530

    sloved probelm 01:nextb 5 prime no

--- problem_solving/02.level/01.problem.c
+++ problem_solving/02.level/01.problem.c
@@ -1,26 +1,35 @@
-# include <stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    int a,n;
-    scanf("%d",&a);
-   
+  int n,i,prime,count=0;
+  scanf("%d",&n);
+  int next = n+1;
+  while(count<5)
+  {
+      prime=1;
+      if(next<2){
+          prime=0;
+         
+      }
+      else
+      {
+          for(i=2;i<next;i++)
+          {
+              if(next%i==0)
+              {
+                  prime=0;
+              }
+          }
+      }
+      if(prime==1)
+      {
+          printf("%d ",i);
+          count++;
+      }
+      next++;
+  }
+  
 
-    for(int i=2;i<a;i++)
-    {
-        if(a%i==0)
-        {
-            printf("Not a prime number");
-            break;
-        }
-        else if(a==1||a==0)
-        {
-            printf("Not a prime number");
-            break;
-        }
-        else
-        {
-            printf("%d is prime number",a);
-        }
-    }
-    
+    return 0;
 }
