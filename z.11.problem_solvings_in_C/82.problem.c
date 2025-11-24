c7a436a8f436440e3d7570e7b28cd065f6e649ce
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-11-24 22:15:28 +0530

    solved problem82:count of digit

--- z.11.problem_solvings_in_C/82.problem.c
+++ z.11.problem_solvings_in_C/82.problem.c
@@ -1,14 +1,20 @@
 # include <stdio.h>
 int main()
 {
-    int a,count,n;
+    int a,count=0,n;
     scanf("%d",&a);
-    a=n;
-    while(a>0)
+    n=a;
+    while(n>0)
     {
-       n=n%10;
+       n=n/10;
        count++;
     }
-    printf("%d",count);
-
+    printf("%d\n",count);
+  if(count==5)
+  {
+    printf("%d is five digit number.",a);
+  }
+  else{
+    printf("%d is not a five digit number it is %d digit number.",a,count);
+  }
 }
