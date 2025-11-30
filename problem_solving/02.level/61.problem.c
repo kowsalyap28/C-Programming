d00b74b4b7934b5d4f885c641eec1e7b9f4d2b11
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-11-30 17:44:51 +0530

    solved problem 61:sum of digits untill it becomes single digit

---
+++ problem_solving/02.level/61.problem.c
@@ -0,0 +1,18 @@
+//Digit Summation
+# include <stdio.h>
+int main()
+{
+    int n,sum;
+    scanf("%d",&n);
+    while(n>9)
+    {
+     sum=0;   
+     while(n>0)
+     {
+     sum=sum+n%10;
+     n/=10;
+     }
+     n=sum;
+    }
+    printf("%d",sum);
+}
