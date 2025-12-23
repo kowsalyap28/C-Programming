150a3d125503663acf2a397d23c0590d09d062c1
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-12-23 23:26:19 +0530

    solved problem 19:remove the duplicate number

---
+++ problem_solving/03.level/19.problem.c
@@ -0,0 +1,31 @@
+// remove the duplicate number
+# include <Stdio.h>
+int main()
+{
+    int n,i,tem,k,j;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                for(k=j;k<n-1;k++)
+                {
+                    a[k]=a[k+1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+}
