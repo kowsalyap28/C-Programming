fb92e127b634e15cfbaf75af2d14fcf86249e1b3
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-01-02 20:38:44 +0530

    solved problem 38:count the repeated value

---
+++ problem_solving/03.level/38.problem.c
@@ -0,0 +1,29 @@
+
+# include <stdio.h>
+int main()
+{
+    int n1,count;
+    scanf("%d",&n1);
+    int a[n1],visited[n1];
+    for(int i=0;i<n1;i++)
+    {
+        scanf("%d",&a[i]);
+        visited[i]=0;
+    }
+    for(int i=0;i<n1;i++)
+    {
+        if(visited[i]==1)
+        continue;
+        int count=1;
+        for(int j=i+1;j<n1;j++)
+        {
+            if(a[i]==a[j])
+            {
+              count++;
+              visited[j]=1;
+            }
+        }
+     printf("%d occur %d times\n",a[i],count);
+    }
+
+}
