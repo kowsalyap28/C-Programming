e625884b9c589aa4fd974d533677aaa319ac0b36
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-01-09 11:41:24 +0530

    sloved problem 47 :sum of array element

---
+++ problem_solving/03.level/47.problem.c
@@ -0,0 +1,18 @@
+//sum of array element 
+# include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    printf("%d",sum);
+}
