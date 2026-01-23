63edf15ec50a9237526299cb58079c16ea9ce4d8
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-01-23 21:41:26 +0530

    solved problem 58:printing in rev order

---
+++ problem_solving/02.level/58.problem.c
@@ -0,0 +1,17 @@
+//printing pattern in reverse order
+# include <stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=n;j>=n;j++)
+        {
+            printf("%d ",j);
+        }
+    
+    }
+   
+}
+
