7d9c9be8ebd870e4158ef92d1c7bd3ce7a76d66a
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-01-24 22:17:52 +0530

    solved problem 65 : pivot integer

---
+++ problem_solving/03.level/65.problem.c
@@ -0,0 +1,25 @@
+//pivot integer
+# include <stdio.h>
+int main()
+{
+    int ns,i;
+    scanf("%d",&ns);
+    int arr[ns];
+    for(i=0;i<ns;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int index;
+    scanf("%d",&index);                    //indes for which integer we want to check
+    int left,sum1=0,sum2=0;
+    for(int i=0;i<index;i++)
+    {     
+       sum1+=arr[i];
+    }
+    for(int i=index+1;i<ns;i++)
+    {
+        sum2+=arr[i];
+    }
+    printf("%d %d",sum1,sum2);
+    return 0;
+}
