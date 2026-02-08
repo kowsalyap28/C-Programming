c77c8dc1844183fc363a83c4725c1ce73a968662
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-02-08 21:14:10 +0530

    solved problem 83:Count Occurrences of a Number

---
+++ problem_solving/03.level/83.problem.c
@@ -0,0 +1,22 @@
+//Count Occurrences of a Number
+# include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+          scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+      {
+        int count=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            count++;
+        }
+        printf("%d ",count);
+      }
+}
