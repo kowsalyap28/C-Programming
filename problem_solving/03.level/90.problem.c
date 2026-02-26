c92e9f08f5b97468b4ea510a00e7052dd35b1179
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-02-26 21:52:36 +0530

    single Number

---
+++ problem_solving/03.level/90.problem.c
@@ -0,0 +1,22 @@
+//single Number
+# include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(arr[i]==arr[j]){
+                count++;
+            }
+        }
+        if(count==1){
+            printf("%d",arr[i]);
+        }
+    }
+   
+}
