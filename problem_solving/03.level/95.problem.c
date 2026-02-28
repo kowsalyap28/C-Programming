84723f8653ab4c8112eea927371c3104ef084eda
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-02-28 22:58:33 +0530

    remove even number in even index

---
+++ problem_solving/03.level/95.problem.c
@@ -0,0 +1,18 @@
+//remove even number in even index
+# include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+     int arr[n];
+     for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+     }
+    for(int i=0;i<n;i++){
+        if(i%2==0&&arr[i]%2==0)
+        {
+          continue;
+        }
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
