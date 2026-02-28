17787df811ad46bc12b99cd2e07b780cbd5b92f3
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-02-28 23:03:29 +0530

    remove the given index

---
+++ problem_solving/03.level/105.problem.c
@@ -0,0 +1,22 @@
+// remove the given index
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int ri; // index to remove
+    scanf("%d", &ri);
+    for (int i = 0; i < n; i++)
+    {
+        if (i == ri)
+        {
+            continue;
+        }
+        printf("%d ", arr[i]);
+    }
+}
