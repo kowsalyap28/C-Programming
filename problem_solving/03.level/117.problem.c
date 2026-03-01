4c5bd0e8a3d78f8d60c898a6db8b53bf4b15ef56
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-03-01 22:31:36 +0530

    replace peak

---
+++ problem_solving/03.level/117.problem.c
@@ -0,0 +1,22 @@
+// 51. Replace the peak number by adding it neighbor elements
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if ((i == 0 || arr[i] >= arr[i - 1]) &&
+            (i == n - 1 || arr[i] >= arr[i + 1]))
+        {
+            arr[i]=arr[i]+arr[i-1]+arr[i+1];
+        
+        }
+        printf("%d ",arr[i]);
+    }
+}
