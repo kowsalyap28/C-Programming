d4e4f3aab7767039c7cb8b7b81759ae23a88affe
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-03-02 21:47:09 +0530

    last repeat

---
+++ problem_solving/03.level/124.problem.c
@@ -0,0 +1,23 @@
+//44. Find Last Repeated Element
+# include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                printf("%d",arr[i]);
+                return 0;
+            }
+        }
+    }
+}
