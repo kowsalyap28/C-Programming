61156a7619f7eb83e00274240dba47643e5585f3
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-03-02 21:47:59 +0530

    remove all 0

---
+++ problem_solving/03.level/126.problem.c
@@ -0,0 +1,25 @@
+//move all 0
+# include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int znc=0;
+    for(int i=0;i<n;i++)
+    {
+       if(arr[i]!=0)
+       {
+       arr[znc]=arr[i];
+       znc++;
+       }
+    }
+    for(int i=0;i<znc;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
