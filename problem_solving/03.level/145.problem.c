71ab7067c5babb9e402bca22b454441eacded990
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-03-10 23:47:22 +0530

    delete dublicate

---
+++ problem_solving/03.level/145.problem.c
@@ -0,0 +1,26 @@
+//to delete duplicate
+# include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],visited[n];
+    for(int i=0;i<n;i++)
+    {
+         scanf("%d",&arr[i]);
+         visited[i]=0;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(visited[i]==1)
+        continue;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                visited[j]=1;
+            }
+        }
+        printf("%d ",arr[i]);
+    }
+}
