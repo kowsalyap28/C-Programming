982d1c049a6407650fa3bf49ad89bf401907ff04
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-03-10 23:48:21 +0530

    count dublicate

---
+++ problem_solving/03.level/147.problem.c
@@ -0,0 +1,32 @@
+//24.count total duplicate elements in array.
+# include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],visited[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        visited[i]=0;
+    }
+    int count=0;
+   for(int i=0;i<n;i++)
+   {
+
+    if(visited[i]==1)
+    continue;
+    for(int j=i+1;j<n;j++)
+    {
+        if(arr[i]==arr[j])
+        {
+          visited[j]=1;
+          count++;
+        }
+    }
+   }
+   if(count>0)
+   printf("%d",count);
+   else
+   printf("no duplicate");
+}
