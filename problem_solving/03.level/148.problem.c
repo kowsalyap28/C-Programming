f7f85f917f2cabea1a09c8c0d35138bf07dacaa8
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-03-10 23:48:41 +0530

    square

---
+++ problem_solving/03.level/148.problem.c
@@ -0,0 +1,16 @@
+//square of each element
+# include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]*arr[i]);
+    }
+}
