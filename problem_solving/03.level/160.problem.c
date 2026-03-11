ec05bc545b40908c741530008c107d05f81783ce
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-03-11 23:02:02 +0530

    largest in matrix

---
+++ problem_solving/03.level/160.problem.c
@@ -0,0 +1,27 @@
+//largest in matrix
+# include <stdio.h>
+int main()
+{
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int largest=arr[0][0];
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(arr[i][j]>largest)
+            {
+                largest=arr[i][j];
+            }
+        }
+    }
+    printf("%d",largest);
+}
