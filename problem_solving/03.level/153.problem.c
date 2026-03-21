648006f62da0ae8079550ed2717539fd0eb17fe1
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-03-21 21:35:43 +0530

    revmove even

---
+++ problem_solving/03.level/153.problem.c
@@ -0,0 +1,22 @@
+//18. To remove EVEN numbers
+# include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],k=0;
+    for(int i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            arr[k]=arr[i];
+            k++;
+        }
+    }
+    for(int i=0;i<k;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
