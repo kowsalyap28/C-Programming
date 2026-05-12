2165086f4f238e8c4e83f185a84e2236cd5d5f38
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-05-12 22:55:46 +0530

    remove duplicate

---
+++ problem_solving/04.level/78.removeDuplicate.c
@@ -0,0 +1,29 @@
+// remove duplicate
+# include <stdio.h>
+# include <string.h>
+int main()
+{
+    char str[100];
+    fgets(str,100,stdin);
+    str[strcspn(str,"\n")]='\0';
+    int len=strlen(str);
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i]==' ')
+        continue;
+       for(int j=i+1;str[j]!='\0';j++)
+       {
+             if(str[i]==str[j])
+             {
+                 for(int k=j;str[k]!=0;k++)
+                 {
+                    str[k]=str[k+1];
+
+                 }
+             j--;
+             len--;
+            }
+       }
+    }
+   printf("%s",str);
+}
