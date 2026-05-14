72fe13778e27be7d6d010969caf33f4ff32d1c0b
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-05-14 19:36:41 +0530

    85.palindrome

---
+++ problem_solving/04.level/85.palindrome.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <string.h>
+int main()
+{
+    char str[100];
+    fgets(str,100,stdin);
+    str[strcspn(str,"\n")]='\0';
+    int len=strlen(str)-1;
+    int found=1;
+    for(int i=0;i<len;i++,len--)
+    {
+        if(str[i]!=str[len])
+        {
+            found=0;
+        }
+    }
+   if(found==1)
+   {
+     printf("%s is palindrom",str);
+   }
+   else
+   {
+     printf("%s is not palindrom",str);
+   }
+}
