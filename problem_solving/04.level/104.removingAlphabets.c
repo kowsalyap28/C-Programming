d04ce7bb26241e5cdf76dde346a86653624f9c46
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-05-17 17:59:59 +0530

    removing alphabets

---
+++ problem_solving/04.level/104.removingAlphabets.c
@@ -0,0 +1,25 @@
+//Removing Alphabets
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main()
+{
+    char str[100];
+    fgets(str,100,stdin);
+    str[strcspn(str,"\n")]='\0';
+    int len=strlen(str);
+    for(int i=0;str[i]!='\0';i++)
+    {
+         char ch=tolower(str[i]);
+         if(ch>='a'&&ch<='z')
+         {
+            for(int j=i;str[j]!='\0';j++)
+            {
+                str[j]=str[j+1];
+            }
+            i--;
+            len--;
+         }
+    }
+    printf("%s",str);
+}
