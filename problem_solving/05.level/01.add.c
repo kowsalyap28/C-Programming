f94853392ed62c9b2a50db368dd5603efbcd4d07
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-05-28 20:10:11 +0530

    add

---
+++ problem_solving/05.level/01.add.c
@@ -0,0 +1,11 @@
+// sample exercise
+# include <stdio.h>
+void greet(char *name){
+    printf("Hello %s!\n",name);
+}
+int main()
+{
+  greet("kowsalya");
+  greet("KOWSALYA");
+  greet("KP.kowsalya");
+}
