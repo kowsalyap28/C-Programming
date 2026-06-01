2e4d34bd50138b6522e9779feb80485715c2f242
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2026-06-01 22:48:38 +0530

    add

---
+++ problem_solving/05.level/02.add.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+// int add()
+// {
+//     int a=3,b=4,c=a+b; // if you not decalre the value in add(a,b) ,you must init the value here
+
+//     return c;
+// }
+// int main()
+// {
+// printf("%d",add());
+// }
+int sum(int k,int m)
+{
+   int v=m-k;
+}
+int main()
+{
+printf("%d",sum(11,28));
+}
